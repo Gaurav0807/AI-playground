39a56cf8a841d4aae85844bc306527c5375aed38
Gaurav Rawat <54076460+Gaurav0807@users.noreply.github.com>
2026-03-08 03:42:00 +0000

    update comment

--- AI-Personal-Finance-Optimization-Agent/infra/api.tf
+++ AI-Personal-Finance-Optimization-Agent/infra/api.tf
@@ -3,14 +3,15 @@ resource "aws_apigatewayv2_api" "agent_api" {
   name          = "agent-api"
   protocol_type = "HTTP"
 }
-
+#This creates an API Gateway HTTP API.
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id                 = aws_apigatewayv2_api.agent_api.id
-  integration_type       = "AWS_PROXY"
+  integration_type       = "AWS_PROXY" # API Gateway forwards request directly to Lambda
   integration_uri        = aws_lambda_function.test_agent.invoke_arn
-  payload_format_version = "2.0"
+  payload_format_version = "2.0" #modern HTTP API format
 }
+# Connects API Gateway → Lambda
 
 #Aws_proxy :- Send Request Directly to Lambda
 #invoke arn :- Lambda Execution link
@@ -18,14 +19,20 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
 resource "aws_apigatewayv2_route" "agent_route" {
   api_id    = aws_apigatewayv2_api.agent_api.id
   route_key = "POST /analyze"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}" # Send this route to the Lambda integration
 }
+#Send this route to the Lambda integration
 
 resource "aws_apigatewayv2_stage" "agent_stage" {
   api_id      = aws_apigatewayv2_api.agent_api.id
   name        = "$default"
-  auto_deploy = true
+  auto_deploy = true #Automatically deploy API when changes happen.
 }
+# Without default stage:
+# https://api-id.execute-api.region.amazonaws.com/dev/analyze
+
+# With $default:
+# https://api-id.execute-api.region.amazonaws.com/analyze
 
 resource "aws_lambda_permission" "api_permission" {
   statement_id  = "AllowAPIGatewayInvoke"
